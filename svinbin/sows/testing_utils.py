7174c93d43e810520244e99471cc3655df0f0256
kaizerj <kzrster@gmail.com>
2019-08-12 14:29:53 +0900

    Sow testing utils fix far id count

--- svinbin/sows/testing_utils.py
+++ svinbin/sows/testing_utils.py
@@ -9,6 +9,7 @@ from piglets.models import NewBornPigletsGroup
 from sows_events.models import Semination, SowFarrow
 from piglets_events.models import NewBornPigletsMerger
 
+FARM_ID_COUNT = 100000
 
 def create_statuses():
     if SowStatus.objects.all().count() < 1:
@@ -31,22 +32,22 @@ def create_sow_and_put_in_workshop_one(section_number=None, cell_number=None):
         if cell_number:
             cell = SowSingleCell.objects.get(section=section, number=cell_number)
             location = Location.objects.get(sowSingleCell=cell)
-            sow = Sow.objects.create(farm_id=random.randint(1, 1000), location=location)
+            sow = Sow.objects.create(farm_id=random.randint(1, FARM_ID_COUNT), location=location)
             cell.sow = sow
             cell.save()
         else:
             location = Location.objects.get(section=section)
-            sow = Sow.objects.create(farm_id=random.randint(1, 1000), location=location)
+            sow = Sow.objects.create(farm_id=random.randint(1, FARM_ID_COUNT), location=location)
     else:
         location = Location.objects.get(workshop=WorkShop.objects.get(number=1))
-        sow = Sow.objects.create(farm_id=random.randint(1, 1000), location=location)
+        sow = Sow.objects.create(farm_id=random.randint(1, FARM_ID_COUNT), location=location)
     return sow
 
 def create_sow_and_put_in_workshop_two(section_number, cell_number):
     section = Section.objects.get(workshop=WorkShop.objects.get(number=2), number=section_number)
     cell = SowGroupCell.objects.get(section=section, number=cell_number)
     location = Location.objects.get(sowGroupCell=cell)
-    sow = Sow.objects.create(farm_id=random.randint(1, 1000), location=location)
+    sow = Sow.objects.create(farm_id=random.randint(1, FARM_ID_COUNT), location=location)
     cell.sows.add(sow)
     return sow
 
@@ -54,7 +55,7 @@ def create_sow_and_put_in_workshop_three(section_number=1, cell_number=1):
     section = Section.objects.get(workshop=WorkShop.objects.get(number=3), number=section_number)
     cell = SowAndPigletsCell.objects.get(section=section, number=cell_number)
     location = Location.objects.get(sowAndPigletsCell=cell)
-    sow = Sow.objects.create(farm_id=random.randint(1, 1000), location=location)
+    sow = Sow.objects.create(farm_id=random.randint(1, FARM_ID_COUNT), location=location)
     return sow
 
 def create_sow_with_semination_and_put_in_workshop_three(week=1, section_number=1, cell_number=1):
@@ -64,7 +65,7 @@ def create_sow_with_semination_and_put_in_workshop_three(week=1, section_number=
     return sow
     
 def create_sow_with_location(location):
-    sow = Sow.objects.create(farm_id=random.randint(1, 1000), location=location)
+    sow = Sow.objects.create(farm_id=random.randint(1, FARM_ID_COUNT), location=location)
     return sow
 
 def create_sow_with_semination(location, week=1):
