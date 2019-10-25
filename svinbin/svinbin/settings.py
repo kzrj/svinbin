aa2b09e4d1ba8bafeb075b09e593c6244b796caa
kaizerj <kzrster@gmail.com>
2019-10-25 18:53:02 +0900

    backstatic

--- svinbin/svinbin/settings.py
+++ svinbin/svinbin/settings.py
@@ -177,7 +177,7 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/backstatic/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 
