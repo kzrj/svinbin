9c06e71a1b40c52dd125b6487d357f23460e328c
kaizerj <kzrster@gmail.com>
2019-11-25 18:14:52 +0900

    create svinbin users to init data

--- svinbin/staff/testing_utils.py
+++ svinbin/staff/testing_utils.py
@@ -15,11 +15,16 @@ def create_employee(farm_name=''):
 
 def create_test_users():
     try:
-        user = User.objects.create_user('test_seminator', 't@t.ru', 'qwerty123')
+        user = User.objects.create_user('test_seminator', 't@t.ru', 'svinbin123')
         WorkShopEmployee.objects.create(user=user, is_seminator=True, is_officer=True)
         return user
     except:
         pass
+
+def create_user(username, farm_name):
+    user = User.objects.create_user(username, 't@t.ru', 'svinbin123')
+    WorkShopEmployee.objects.create(user=user, is_seminator=True, is_officer=False, farm_name=farm_name)
+    return user
 
 
 def create_workshop_user(username, password, ws_number, is_seminator=False, is_officer=False):
@@ -28,3 +33,12 @@ def create_workshop_user(username, password, ws_number, is_seminator=False, is_o
     WorkShopEmployee.objects.create(user=user, workshop=workshop, \
         is_seminator=is_seminator, is_officer=is_officer)
     return user
+
+
+def create_svinbin_users():
+    User.objects.create_superuser(username='kaizerj',email='kzrster@gmail.com', password='svinbin123')
+    create_user('shmigina', 'ШМЫГИ')
+    create_user('borisov', 'БОРИС')
+    create_user('semenova', 'СЕМЕН')
+    create_user('gary', 'ГАРИ')
+    create_user('ivanov', 'ИВАНО')
