6cc8645e8ccae3ba2e70d77da123361c3c4a592a
kaizerj <kzrster@gmail.com>
2019-12-04 12:46:13 +0900

    docker compose

--- svinbin/svinbin/urls.py
+++ svinbin/svinbin/urls.py
@@ -25,40 +25,23 @@ from workshopthree import views as workshopthree_views
 router = routers.DefaultRouter()
 
 # transactions
-router.register(r'sowtransactions/workshops', transaction_views.WorkShopOneTwoSowTransactionViewSet,
-    basename='workshop-sowtransactions')
-router.register(r'sowtransactions/transactions', transaction_views.SowTransactionsViewSet,
+router.register(r'transactions/sowtransactions', transaction_views.SowTransactionsViewSet,
     basename='sowtransactions')
+router.register(r'transactions/pigletstransactions', transaction_views.PigletsTransactionsViewSet,
+    basename='pigletstransactions')
 
 # pigs
 router.register(r'sows', sows_views.SowViewSet, basename='sows')
 router.register(r'boars', sows_views.BoarViewSet, basename='boars')
+
+# piglets
+router.register(r'piglets', piglets_views.PigletsViewSet, basename='piglets')
 
 # events
 router.register(r'sows_events/seminations', sows_events_views.SeminationViewSet, basename='seminations')
 router.register(r'sows_events/ultrasounds', sows_events_views.UltrasoundViewSet, basename='ultrasounds')
 router.register(r'sows_events/farrows', sows_events_views.SowFarrowViewSet, basename='farrows')
 router.register(r'sows_events/spec_uboi', sows_events_views.CullingSowViewSet, basename='spec_uboi')
-
-# cells
-# router.register(r'pigletsgroupcells', locations_views.PigletsGroupCellViewSet, basename='pigletsgroupcell')
-
-# by workshops
-# workshop one two
-router.register(r'workshoponetwo/sows', workshoponetwo_views.WorkShopOneTwoSowViewSet, \
- basename='workshoponetwo-sows')
-
-# workshop three
-# router.register(r'workshopthree/newbornpiglets', workshopthree_views.WorkShopThreeNewBornPigletsViewSet, \
- # basename='workshopthree-piglets')
-router.register(r'workshopthree/sows', workshopthree_views.WorkShopThreeSowsViewSet, \
- basename='workshopthree-sows')
-router.register(r'workshopthree/wsinfo', workshopthree_views.WorkshopInfo, \
- basename='workshopthree-info')
-
-# workshop four
-# router.register(r'workshopfour/piglets', workshopfour_views.WorkShopFourPigletsViewSet, \
-#  basename='workshopfour-piglets')
 
 # location
 router.register(r'locations', locations_views.LocationViewSet, \
@@ -74,12 +57,16 @@ router.register(r'tours', tours_views.TourViewSet, \
 router.register(r'users', staff_views.UsersViewSet, \
  basename='users')
 
-# piglets
-router.register(r'piglets', piglets_views.PigletsViewSet, basename='piglets')
+# by workshops
+# workshop one two
+router.register(r'workshoponetwo/sows', workshoponetwo_views.WorkShopOneTwoSowViewSet, \
+ basename='workshoponetwo-sows')
 
-# inits
-# router.register(r'inits', piglets_views.WorkShopNomadPigletsViewSet, \
- # basename='piglets')
+# workshop three
+router.register(r'workshopthree/sows', workshopthree_views.WorkShopThreeSowsViewSet, \
+ basename='workshopthree-sows')
+router.register(r'workshopthree/wsinfo', workshopthree_views.WorkshopInfo, \
+ basename='workshopthree-info')
 
 schema_view = get_swagger_view(title='API Docs')
 
