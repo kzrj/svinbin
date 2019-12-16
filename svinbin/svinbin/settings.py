48b187d879fe9e9f752b6519bc89922b4e3e99ff
kaizerj <kzrster@gmail.com>
2019-12-16 11:40:58 +0900

    frontend

--- svinbin/svinbin/settings.py
+++ svinbin/svinbin/settings.py
@@ -163,7 +163,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-US'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Irkutsk'
 
