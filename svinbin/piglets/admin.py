3e09f693682b9741c95678d98eb7bf4ebf5ae080
kaizerj <kzrster@gmail.com>
2020-01-14 18:37:29 +0900

    admin piglets Status

--- svinbin/piglets/admin.py
+++ svinbin/piglets/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from piglets.models import Piglets
+from piglets.models import Piglets, PigletsStatus
 
 
 @admin.register(Piglets)
 class PigletsAdmin(admin.ModelAdmin):
     list_display = [f.name for f in Piglets._meta.fields]
+
+
+@admin.register(PigletsStatus)
+class PigletsStatusAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in PigletsStatus._meta.fields]
