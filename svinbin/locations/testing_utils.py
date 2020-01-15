b574c719a6854efa7fecd16faad208e6a786949a
kaizerj <kzrster@gmail.com>
2020-01-15 14:47:00 +0900

    actual sections and cells

--- svinbin/locations/testing_utils.py
+++ svinbin/locations/testing_utils.py
@@ -120,7 +120,7 @@ def init_ws_8():
             init_piglets_cells(section, 4)
 
 def init_ws_567():
-    for workshop in WorkShop.objects.filter(number__in=[5, 6, 7])
+    for workshop in WorkShop.objects.filter(number__in=[5, 6, 7]):
         init_sections(workshop, 4)
 
         for section in Section.objects.filter(workshop=workshop):
