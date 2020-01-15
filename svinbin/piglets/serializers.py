00303daa830b9ace0b48c5e2aa243c2644373811
kaizerj <kzrster@gmail.com>
2020-01-15 16:03:25 +0900

    piglets move view add gilts contains

--- svinbin/piglets/serializers.py
+++ svinbin/piglets/serializers.py
@@ -35,6 +35,6 @@ class MergeFromListSerializer(serializers.Serializer):
 
 class MovePigletsSerializer(serializers.Serializer):
     to_location = serializers.PrimaryKeyRelatedField(queryset=Location.objects.all())
-    new_amount = serializers.IntegerField(required=False, required=False, allow_null=True)
+    new_amount = serializers.IntegerField(required=False)
     gilts_contains = serializers.BooleanField(default=False, required=False, allow_null=True)
     merge = serializers.BooleanField(default=False)
