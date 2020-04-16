bd402ee4780eac678d8ce4dc4350af78d5f9361e
kaizerj <kzrster@gmail.com>
2020-04-16 14:39:09 +0900

    bug age

--- svinbin/reports/views.py
+++ svinbin/reports/views.py
@@ -47,13 +47,14 @@ class TourReportViewSet(viewsets.ModelViewSet):
                 .add_weight_data_not_mixed() \
                 .add_weight_data_mixed() \
                 .add_avg_weight_data() \
-                .add_weight_date() \
-                .add_age_at_weight_date() \
                 .add_culling_weight_not_mixed_piglets() \
                 .add_culling_qnty_not_mixed_piglets() \
                 .add_culling_avg_weight_not_mixed_piglets() \
                 .add_culling_percentage_not_mixed_piglets() \
                 .add_current_piglets_age()
+                # .add_weight_date() \
+                # .add_age_at_weight_date() \
+                
     serializer_class = ReportTourSerializer
     filter_class = TourFilter
 
