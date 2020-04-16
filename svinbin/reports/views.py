b142a2d0bf75cd8e5bfc6db7d70a194257c4a06e
kaizerj <kzrster@gmail.com>
2020-04-16 21:03:31 +0900

    reports

--- svinbin/reports/views.py
+++ svinbin/reports/views.py
@@ -42,7 +42,9 @@ class TourReportViewSet(viewsets.ModelViewSet):
     queryset = Tour.objects.all() \
                 .add_sow_data() \
                 .add_farrow_data() \
-                .add_weight_data_not_mixed() \
+                .add_current_not_mixed_piglets_quantity() \
+                # .add_current_mixed_piglets_quantity() \
+                # .add_weight_data_not_mixed() \
                 # .add_weight_data_mixed() \
                 # .add_avg_weight_data() \
                 # .add_culling_weight_not_mixed_piglets() \
