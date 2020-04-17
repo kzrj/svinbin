236944d6ef58021cee7e89c2bce0eb7a9f277457
kaizerj <kzrster@gmail.com>
2020-04-17 15:05:09 +0900

    reports

--- svinbin/reports/views.py
+++ svinbin/reports/views.py
@@ -50,9 +50,9 @@ class TourReportViewSet(viewsets.ModelViewSet):
                 .add_culling_percentage_not_mixed_piglets() \
                 .add_weight_date() \
                 .add_count_tour_sow() \
-                .add_culling_data_by_ws() \
                 .add_current_piglets_age() \
                 .add_age_at_weight_date() \
+                # .add_culling_data_by_ws() \
 
     serializer_class = ReportTourSerializer
     filter_class = TourFilter
