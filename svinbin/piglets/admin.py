3144a15b39e869ae8baefd7c9ae02bab15b873d8
kaizerj <kzrster@gmail.com>
2020-04-22 12:30:58 +0900

    piglets admin

--- svinbin/piglets/admin.py
+++ svinbin/piglets/admin.py
@@ -7,6 +7,12 @@ from piglets.models import Piglets, PigletsStatus
 class PigletsAdmin(admin.ModelAdmin):
     list_display = [f.name for f in Piglets._meta.fields]
 
+    def get_queryset(self, request):
+        qs = super(PigletsAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs.get_all()
+        return qs.get_all()
+
 
 @admin.register(PigletsStatus)
 class PigletsStatusAdmin(admin.ModelAdmin):
