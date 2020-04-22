5d4c67c46c45f2482664be1e67bf0e3013317409
kaizerj <kzrster@gmail.com>
2020-04-22 12:47:37 +0900

    piglets admin

--- svinbin/piglets/admin.py
+++ svinbin/piglets/admin.py
@@ -8,7 +8,7 @@ from piglets.models import Piglets, PigletsStatus
 class PigletsAdmin(admin.ModelAdmin):
     list_display = [f.name for f in Piglets._meta.fields]
     list_filter = ('active', 'modified_at',)
-    search_fields = ['location',]
+    search_fields = ['location__id',]
 
     def get_queryset(self, request):
         qs = super(PigletsAdmin, self).get_queryset(request)
