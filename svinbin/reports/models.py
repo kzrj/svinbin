9d4436e10cc3e660ed86a1dd89301f88731c8e8e
kaizerj <kzrster@gmail.com>
2020-04-30 16:48:40 +0900

    dir rep filter

--- svinbin/reports/models.py
+++ svinbin/reports/models.py
@@ -285,7 +285,7 @@ class ReportDateQuerySet(models.QuerySet):
                 total_spec=Sum('piglets_spec_qnty'),
                 total_prirezka=Sum('piglets_prirezka_qnty'),
                 total_spec_weight=Sum('piglets_spec_total_weight'),
-                total_pigs=(pigs_count + F('pk') - F('pk'))
+                # total_pigs=(pigs_count + F('pk') - F('pk'))
                 )
 
 
