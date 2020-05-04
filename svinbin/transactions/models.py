d6bd235139cfe36450c2f3067275baab747dc485
kaizerj <kzrster@gmail.com>
2020-05-04 16:07:07 +0900

    sow tr fix

--- svinbin/transactions/models.py
+++ svinbin/transactions/models.py
@@ -31,6 +31,7 @@ class SowTransactionManager(CoreModelManager):
 
         print('To_location', to_location)
         print('To_location workshop', to_location.workshop)
+        print('________________________________________@@@@@@@@@@@@@@_')
 
         if sow.status.title == 'Опоросилась' and to_location.workshop:
             if to_location.workshop.number == 1  and sow.location.sowAndPigletsCell:
