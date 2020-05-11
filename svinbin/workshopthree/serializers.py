3ab725246942e2ea46ae92fcb5716b0b84d77ae0
kaizerj <kzrster@gmail.com>
2020-05-11 19:29:24 +0900

    Cut excess code from ws3 app

--- svinbin/workshopthree/serializers.py
+++ svinbin/workshopthree/serializers.py
@@ -1,27 +1,8 @@
 # -*- coding: utf-8 -*-
 from rest_framework import serializers
 
-import transactions.serializers as transactions_serializers
-import sows.serializers as sows_serializers
-from sows.models import Sow
 from tours.models import Tour
-
-
-class SowsIdsSerializer(serializers.Serializer):
-    sows = serializers.PrimaryKeyRelatedField(queryset=Sow.objects.all(), many=True)
-
-
-class NewGiltBirthIdSerializer(serializers.Serializer):
-    birth_id = serializers.CharField()
-
-
-class CreateRecountSerializer(serializers.Serializer):
-    quantity = serializers.IntegerField()
 
 
 class MarkSowAsNurseSerializer(serializers.Serializer):
     piglets_tour = serializers.PrimaryKeyRelatedField(queryset=Tour.objects.all(), required=False)
-
-
-class ImportJsonFile(serializers.Serializer):
-    file = serializers.FileField(max_length=None, use_url=False)
--- svinbin/workshopthree/views.py
+++ svinbin/workshopthree/views.py
@@ -7,7 +7,7 @@ from workshopthree import serializers
 from sows import serializers as sows_serializers
 from sows_events import serializers as sows_events_serializers
 
-from sows.models import Sow, Gilt
+from sows.models import Sow
 from sows_events import models as sows_events_models
 
 from sows.views import WorkShopSowViewSet
@@ -50,37 +50,3 @@ class WorkShopThreeSowsViewSet(WorkShopSowViewSet):
                 status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    @action(methods=['post'], detail=True)
-    def create_gilt(self, request, pk=None):
-        serializer = serializers.NewGiltBirthIdSerializer(data=request.data)
-        if serializer.is_valid():
-            sow = self.get_object()
-            Gilt.objects.create_gilt(serializer.validated_data['birth_id'], sow)
-
-            return Response(
-                {
-                 "message": f'Ремонтная свинка с номером {serializer.validated_data["birth_id"]} \
-                        успешно создана.',
-                },
-                status=status.HTTP_200_OK)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-    @action(methods=['post'], detail=False)
-    def import_from_farm_json(self, request):
-        serializer = serializers.ImportJsonFile(data=request.data)
-        if serializer.is_valid():
-            sows_created, sows_passed = \
-                import_farm.import_from_json_to_ws2_3(serializer.validated_data['file'], 3, request.user)
-
-            return Response(
-            {
-                "sows_created": len(sows_created),
-                "sows_passed": len(sows_passed),
-                "message": "Файл загружен и обработан."
-            },
-            status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
