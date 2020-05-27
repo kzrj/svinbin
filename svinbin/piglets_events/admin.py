e2ec764add91f8e42df24a26abd17471db8ccb85
kaizerj <kzrster@gmail.com>
2020-05-27 11:22:46 +0900

    piglets events admin

--- svinbin/piglets_events/admin.py
+++ svinbin/piglets_events/admin.py
@@ -5,23 +5,23 @@ from piglets_events import models
 
 @admin.register(models.WeighingPiglets)
 class WeighingPigletsAdmin(admin.ModelAdmin):
-	search_fields = ['piglets_group__id']
+    search_fields = ['piglets_group__id']
     list_display = [f.name for f in models.WeighingPiglets._meta.fields]
 
 
 @admin.register(models.CullingPiglets)
 class CullingPigletsAdmin(admin.ModelAdmin):
-	search_fields = ['piglets_group__id']
+    search_fields = ['piglets_group__id']
     list_display = [f.name for f in models.CullingPiglets._meta.fields]
 
 
 @admin.register(models.PigletsSplit)
 class PigletsSplitAdmin(admin.ModelAdmin):
-	# search_fields = ['sow__farm_id']
+    # search_fields = ['sow__farm_id']
     list_display = [f.name for f in models.PigletsSplit._meta.fields]
 
 
 @admin.register(models.PigletsMerger)
 class PigletsMergerAdmin(admin.ModelAdmin):
-	# search_fields = ['sow__farm_id']
+    # search_fields = ['sow__farm_id']
     list_display = [f.name for f in models.PigletsMerger._meta.fields]
