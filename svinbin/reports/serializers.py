f4642cfb87961ebde7f3735e0d61ac79ec620c45
kaizerj <kzrster@gmail.com>
2020-06-22 14:06:35 +0800

    ws3 report view

--- svinbin/reports/serializers.py
+++ svinbin/reports/serializers.py
@@ -46,7 +46,7 @@ class ReportTourSerializer(AnnotateFieldsModelSerializer, serializers.ModelSeria
 
 
 class ReportDateWs3Serializer(AnnotateFieldsModelSerializer, serializers.ModelSerializer):
-    count_sows_ws3_start_date2 = serializers.JSONField(source='count_sows_ws3_start_date')
+    # count_sows_ws3_start_date2 = serializers.JSONField(source='count_sows_ws3_start_date')
     count_sows_ws3_start_date = serializers.ReadOnlyField()
 
     class Meta:
