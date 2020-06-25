fd60372290445c48f981b736c9358581e2181950
kaizerj <kzrster@gmail.com>
2020-06-25 10:57:28 +0800

    xlwriter to xlwings

--- svinbin/transactions/models.py
+++ svinbin/transactions/models.py
@@ -32,7 +32,7 @@ class SowTransactionManager(CoreModelManager):
                 sow_status=sow.status
                 )
 
-        if sow.status and sow.status.title == 'Опоросилась' or sow.status.title == 'Отьем' \
+        if sow.status and (sow.status.title == 'Опоросилась' or sow.status.title == 'Отьем') \
          and to_location.workshop:
             if to_location.workshop.number == 1  and sow.location.sowAndPigletsCell:
                 sow.tour = None
