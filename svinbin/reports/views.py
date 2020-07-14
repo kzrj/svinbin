2e67cd1a6176e33267d2748535dcdbc154ed2b23
kaizerj <kzrster@gmail.com>
2020-07-14 14:21:53 +0800

    export to excel ws report 567

--- svinbin/reports/views.py
+++ svinbin/reports/views.py
@@ -121,8 +121,8 @@ class ReportDateViewSet(viewsets.ModelViewSet):
         queryset = ReportDate.objects.all()\
                             .add_ws_count_piglets_start_day(ws_locs=ws_locs) \
                             .add_ws_weighing_in(ws_number=ws_number) \
-                            .add_ws_weighing_out(ws_number=ws_number) \
-                            # .add_ws_piglets_trs_in_out(ws_locs=ws_locs) \
+                            # .add_ws_weighing_out(ws_number=ws_number) \
+                            .add_ws_piglets_trs_in_out(ws_locs=ws_locs) \
                             # .add_ws_piglets_culling_data(ws_locs=ws_locs) \
 
         queryset = self.filter_queryset(queryset)
