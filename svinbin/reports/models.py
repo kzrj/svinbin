c6c9bd3e696e1b82b38cbd2af7d574920b841f1f
kaizerj <kzrster@gmail.com>
2020-07-14 13:49:28 +0800

    export to excel ws report 567

--- svinbin/reports/models.py
+++ svinbin/reports/models.py
@@ -534,7 +534,7 @@ class ReportDateQuerySet(models.QuerySet):
 
                 total_tr_out_aka_weight_qnty=Sum('tr_out_aka_weight_qnty'),
                 total_tr_out_aka_weight_total=Sum('tr_out_aka_weight_total'),
-                avg_tr_out_weight=Avg('tr_out_aka_weight_avg'),
+                avg_tr_out_weight=Avg('tr_out_aka_weight_avg', output_field=models.FloatField()),
 
                 total_piglets_padej_qnty=Sum('piglets_padej_qnty'),
                 total_piglets_padej_weight=Sum('piglets_padej_weight'),
