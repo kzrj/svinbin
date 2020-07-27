a7f816d364213bff8b2b7dbcc02e7aa3c8ce09d2
kaizerj <kzrster@gmail.com>
2020-07-27 12:34:21 +0800

    admin pigletsgroupcell

--- svinbin/locations/admin.py
+++ svinbin/locations/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from locations.models import Location
+from locations.models import Location, PigletsGroupCell
 
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
     list_display = [f.name for f in Location._meta.fields]
+
+
+@admin.register(PigletsGroupCell)
+class PigletsGroupCellAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in PigletsGroupCell._meta.fields]
