1d62dc51bf170c21230e11d01d8cb41871b3ebd6
kaizerj <kzrster@gmail.com>
2020-09-27 16:29:42 +0800

    piglets manager

--- svinbin/piglets/models.py
+++ svinbin/piglets/models.py
@@ -70,6 +70,14 @@ class PigletsQuerySet(models.QuerySet):
 
 
 class PigletsManager(CoreModelManager):
+    def create(self, *args, kwargs):
+        try:
+            super(PigletsManager, self).create(*args, **kwargs)
+        except:
+            last_pk = self.get_all().order_by('-pk').first().pk
+            kwargs['id'] = last_pk + 1
+            super(PigletsManager, self).create(*args, **kwargs,)
+
     def get_queryset(self):
         return PigletsQuerySet(self.model, using=self._db).select_related('metatour').active()
 
