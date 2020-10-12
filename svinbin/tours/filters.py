d17253dd4edd0b789f06475d801de5b717c7555d
kaizerj <kzrster@gmail.com>
2020-10-12 18:07:59 +0800

    tour filter

--- svinbin/tours/filters.py
+++ svinbin/tours/filters.py
@@ -11,10 +11,22 @@ from sows.models import Sow
 
 class TourFilter(filters.FilterSet):
     by_workshop_number = filters.NumberFilter(method='filter_by_workshop_number')
+    has_weights = filters.BooleanFilter(method='filter_has_weights')
 
     def filter_by_workshop_number(self, queryset, name, value):
         pks = Sow.objects.all().get_tours_pks(workshop_number=value)
         return queryset.filter(pk__in=pks)
+
+    def filter_has_weights(self, queryset, name, value):
+        if value:
+        	return queryset.filter(Q(
+        			Q(first_date_3_4__isnull=False) |
+        			Q(first_date_4_8__isnull=False) |
+        			Q(first_date_8_5__isnull=False) |
+        			Q(first_date_8_6__isnull=False) |
+        			Q(first_date_8_7__isnull=False)
+        		))
+        return queryset.none()
 
     class Meta:
         model = Tour
