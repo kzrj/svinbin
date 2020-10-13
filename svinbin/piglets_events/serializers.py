b0709a892ff9db3c73389f8ab3bf2fb463fc7c21
kaizerj <kzrster@gmail.com>
2020-10-13 18:13:38 +0800

    add to reportTourV2 farrow data and culling data

--- svinbin/piglets_events/serializers.py
+++ svinbin/piglets_events/serializers.py
@@ -48,6 +48,8 @@ class WeighingPigletsSerializer(serializers.ModelSerializer):
 
 
 class WeighingPigletsReadSerializer(serializers.ModelSerializer):
+    date = serializers.DateField(source='date__date')
+    
     class Meta:
         model = WeighingPiglets
         fields = ['date', 'total_weight', 'average_weight', 'piglets_quantity', 'piglets_age']
