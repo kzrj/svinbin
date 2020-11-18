9df2e548d941a8f8423efb6f3ec0385587e45374
kaizerj <kzrster@gmail.com>
2020-11-18 16:40:32 +0800

    tour filter

--- svinbin/tours/filters.py
+++ svinbin/tours/filters.py
@@ -16,7 +16,7 @@ class TourFilter(filters.FilterSet):
     has_weights_in_range = filters.DateFromToRangeFilter(method='filter_has_weights_in_range')
     has_weights_in_ws = filters.CharFilter(method='filter_has_weights_in_ws')
     year = filters.NumberFilter(field_name="year", lookup_expr='exact')
-    last_5 = filters.BooleanFilter(method='filter_last_5')
+    last_n = filters.NumberFilter(method='filter_last_n')
 
     def filter_by_workshop_number(self, queryset, name, value):
         pks = Sow.objects.all().get_tours_pks(workshop_number=value)
@@ -48,9 +48,9 @@ class TourFilter(filters.FilterSet):
     def filter_has_weights_in_ws(self, queryset, name, value):
         return queryset.filter(piglets_weights__place=value)
 
-    def filter_last_5(self, queryset, name, value):
+    def filter_last_n(self, queryset, name, value):
         if value:
-            pks = queryset.values_list('pk', flat=True)[:5]
+            pks = queryset.values_list('pk', flat=True)[:value]
             return queryset.filter(pk__in=pks)
         return queryset
 
