4be5ec9a868e8f7d7f193145faa4518b6b3ea88d
kaizerj <kzrster@gmail.com>
2020-12-09 12:59:29 +0800

    culling piglets admin

--- svinbin/piglets_events/admin.py
+++ svinbin/piglets_events/admin.py
@@ -4,20 +4,20 @@ from piglets_events import models
 from piglets.models import Piglets
 
 
-class PigletsEventFormMixin(admin.ModelAdmin):
+class PigletsEventFormMixin(object):
     def render_change_form(self, request, context, *args, **kwargs):
         context['adminform'].form.fields['piglets_group'].queryset = \
             Piglets.objects.get_all()
         return super(PigletsEventFormMixin, self).render_change_form(request, context, *args, **kwargs)
 
 
-@admin.register(models.WeighingPiglets, PigletsEventFormMixin)
+@admin.register(models.WeighingPiglets)
 class WeighingPigletsAdmin(admin.ModelAdmin):
     search_fields = ['piglets_group__id']
     list_display = [f.name for f in models.WeighingPiglets._meta.fields]
 
 
-@admin.register(models.CullingPiglets, PigletsEventFormMixin)
+@admin.register(PigletsEventFormMixin, models.CullingPiglets)
 class CullingPigletsAdmin(admin.ModelAdmin):
     search_fields = ['piglets_group__id']
     list_display = [f.name for f in models.CullingPiglets._meta.fields]
@@ -42,6 +42,6 @@ class PigletsMergerAdmin(admin.ModelAdmin):
 
 
 @admin.register(models.Recount)
-class RecountAdmin(admin.ModelAdmin, PigletsEventFormMixin):
+class RecountAdmin(admin.ModelAdmin):
     search_fields = ['piglets__id']
     list_display = [f.name for f in models.Recount._meta.fields]
