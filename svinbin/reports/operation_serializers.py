3a76a0959d129dfa7a5ef64101ebba790f8aaa57
kzrj <kzrster@gmail.com>
2021-01-20 13:16:59 +0800

    ptransactions admin

--- svinbin/reports/operation_serializers.py
+++ svinbin/reports/operation_serializers.py
@@ -139,7 +139,7 @@ class OpPigletsWeighingSerializer(OpPigletsEventSerializer):
 class OpPigletsToSowSerializer(OpPigletsEventSerializer):
     quantity = serializers.ReadOnlyField(source='piglets.quantity')
     from_location = serializers.ReadOnlyField(source='piglets.location.get_full_loc')
-    week_tour = serializers.ReadOnlyField(source='metatour.week_tour.str')
+    week_tour = serializers.StringRelatedField(source='metatour.week_tour')
 
     age_at = serializers.SerializerMethodField()
 
