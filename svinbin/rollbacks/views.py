1c3ec6068945351b478ec6e8a48611ee1a0a3da2
kzrj <kzrster@gmail.com>
2021-02-03 14:54:40 +0800

    sow rollback fix

--- svinbin/rollbacks/views.py
+++ svinbin/rollbacks/views.py
@@ -59,8 +59,7 @@ class RollbackViewSet(viewsets.ModelViewSet):
             rollback = Rollback.objects.create_abort_rollback(event_pk=event_pk,
               initiator=request.user, operation_name=operation_name)
 
-        if operation_name == 'ws1_culling' or operation_name == 'ws2_culling' \
-           or operation_name == 'ws3_culling':
+        if 'sow_culling' in operation_name:
             rollback = Rollback.objects.create_sow_culling_rollback(event_pk=event_pk,
               initiator=request.user, operation_name=operation_name)
 
