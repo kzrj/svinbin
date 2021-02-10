26f3395bc64e2bce51739a1e8f021184a2c76433
kzrj <kzrster@gmail.com>
2021-02-10 16:39:22 +0800

    add veterinary

--- svinbin/veterinary/admin.py
+++ svinbin/veterinary/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+
+from veterinary.models import Recipe, Drug
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Recipe._meta.fields]
+
+
+@admin.register(Drug)
+class DrugAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Drug._meta.fields]
