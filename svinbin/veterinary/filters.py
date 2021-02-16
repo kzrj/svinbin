c4defec5897da826485f9974388f6f8a660dae1c
kzrj <kzrster@gmail.com>
2021-02-16 15:06:20 +0800

    pve filters

--- svinbin/veterinary/filters.py
+++ svinbin/veterinary/filters.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.db.models import Q
+
 from django_filters import rest_framework as filters
 
 from veterinary.models import Recipe, PigletsVetEvent
@@ -15,11 +17,17 @@ class RecipeFilter(filters.FilterSet):
 
 class PigletsVetEventFilter(filters.FilterSet):
     ru_type = filters.CharFilter(method='filter_ru_type')
-    sections = NumberInFilter(field_name='location__id', lookup_expr='in')
+    sections = NumberInFilter(method='filter_sections')
     tours = NumberInFilter(field_name='week_tour__id', lookup_expr='in')
 
     def filter_ru_type(self, queryset, name, value):
         return queryset.filter(recipe__med_type=value)
+
+    def filter_sections(self, queryset, name, value):
+        return queryset.filter(Q(
+            Q(location__pigletsGroupCell__section__pk__in=value) |
+            Q(location__sowAndPigletsCell__section__pk__in=value)
+            ))
 
     class Meta:
         model = PigletsVetEvent
