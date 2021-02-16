081072a385538b294bf36e773478c325d72a7744
kzrj <kzrster@gmail.com>
2021-02-16 14:26:37 +0800

    pve filters

--- svinbin/veterinary/filters.py
+++ svinbin/veterinary/filters.py
@@ -14,11 +14,11 @@ class RecipeFilter(filters.FilterSet):
 
 
 class PigletsVetEventFilter(filters.FilterSet):
-	ru_type = filters.CharFilter(method='filter_ru_type')
-	sections = NumberInFilter(field_name='location__id', lookup_expr='in')
-	tours = NumberInFilter(field_name='week_tour__id', lookup_expr='in')
+    ru_type = filters.CharFilter(method='filter_ru_type')
+    sections = NumberInFilter(field_name='location__id', lookup_expr='in')
+    tours = NumberInFilter(field_name='week_tour__id', lookup_expr='in')
 
-	def filter_ru_type(self, queryset, name, value):
+    def filter_ru_type(self, queryset, name, value):
         return queryset.filter(recipe__med_type=value)
 
     class Meta:
