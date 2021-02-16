e626cfca9a32232da83fc5b5626db6b547108909
kzrj <kzrster@gmail.com>
2021-02-16 13:23:53 +0800

    pve filters

--- svinbin/veterinary/views.py
+++ svinbin/veterinary/views.py
@@ -1,6 +1,7 @@
 # # -*- coding: utf-8 -*-
 from rest_framework import status, viewsets
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from locations.models import Section
 from locations.serializers import SectionFilterSerializer
