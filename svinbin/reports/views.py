b1a16c659db612df582e2bb39a163c4170d35a1d
kzrj <kzrster@gmail.com>
2021-03-15 16:04:30 +0800

    ws tours report rem

--- svinbin/reports/views.py
+++ svinbin/reports/views.py
@@ -401,8 +401,8 @@ class ReportWSInfoView(viewsets.ViewSet):
             .add_section_fullness()
 
         places = self.gen_places(ws_number=ws_number)
-        tours = Tour.objects.filter(piglets_weights__place__in=places) \
-                    .distinct()
+        tours = Tour.objects.filter(piglets_weights__place__in=places) 
+                    # .distinct()
 
         if ws_number in [5, 6, 7]:
             tours = tours.add_remont_trs_out(ws_numbers=[ws_number,])
