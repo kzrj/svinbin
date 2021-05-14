e78850aeefcde60c0fcd18cdebe4cd5110b4cc4b
kzrj <kzrster@gmail.com>
2021-05-14 10:54:26 +0800

    front + vetperms

---
+++ frontend/precache-manifest.2790015055291682bbd90f8cd725ca29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e0fca0dc61ec34696dd7d54ab4bad54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b436352e3c0f51821ea7",
+    "url": "/static/js/2.bd5019d9.chunk.js"
+  },
+  {
+    "revision": "67360280b1e728771bc49dea7ac92e3c",
+    "url": "/static/js/2.bd5019d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5c54df609bd2550c414",
+    "url": "/static/js/main.1f63fffc.chunk.js"
+  },
+  {
+    "revision": "00009d080079d6d0541c",
+    "url": "/static/js/runtime-main.d65abb89.js"
+  }
+]);
